0c1113629ceb78191f077e50ed16cc40f5e275c9
Jorge-David-Reyes <jorge.reyes.1650@gmail.com>
2026-09-15 19:32:39 -0600

    cambio en contact

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -6,16 +6,6 @@ import sqlImg from '@/assets/certificates/sql-postgresql.webp';
 
 export const certificates = [
   {
-    title: 'Introduction to Data Engineering',
-    issuer: 'Coursera',
-    date: 'Julio 2026',
-    image: introDataEngImg,
-    links: [
-      { label: 'Coursera', icon: 'simple-icons:coursera', url: 'https://coursera.org/share/b09e043c74b1a2d52dfa2e800ed1cf9a' },
-      { label: 'Credly', icon: 'simple-icons:credly', url: 'https://www.credly.com/badges/a67e4602-bcf9-4d7c-9b9e-bb3a8140f056/public_url' },
-    ],
-  },
-  {
     title: 'Python Project for Data Engineering',
     issuer: 'Coursera',
     date: 'Agosto 2026',
@@ -26,16 +16,6 @@ export const certificates = [
     ],
   },
   {
-    title: 'Introduction to Relational Databases (RDBMS)',
-    issuer: 'Coursera',
-    date: 'Septiembre 2026',
-    image: rdbmsImg,
-    links: [
-      { label: 'Coursera', icon: 'simple-icons:coursera', url: 'https://coursera.org/share/4a6b1438b139edbfb3901335c50a5816' },
-      { label: 'Credly', icon: 'simple-icons:credly', url: 'https://www.credly.com/badges/b9afaee6-3ac3-4d4f-9701-43d745c04a88/public_url' },
-    ],
-  },
-  {
     title: 'Python for Data Science, AI & Development',
     issuer: 'Coursera',
     date: 'Julio 2026',
@@ -43,6 +23,26 @@ export const certificates = [
     links: [
       { label: 'Coursera', icon: 'simple-icons:coursera', url: 'https://coursera.org/share/dcc0f3f1473765cedc4cd021b6017878' },
       { label: 'Credly', icon: 'simple-icons:credly', url: 'https://www.credly.com/badges/bbf78109-09c0-48f6-bd1a-58d5cc06c702/public_url' },
+    ],
+  },
+  {
+    title: 'Introduction to Data Engineering',
+    issuer: 'Coursera',
+    date: 'Julio 2026',
+    image: introDataEngImg,
+    links: [
+      { label: 'Coursera', icon: 'simple-icons:coursera', url: 'https://coursera.org/share/b09e043c74b1a2d52dfa2e800ed1cf9a' },
+      { label: 'Credly', icon: 'simple-icons:credly', url: 'https://www.credly.com/badges/a67e4602-bcf9-4d7c-9b9e-bb3a8140f056/public_url' },
+    ],
+  },
+  {
+    title: 'Introduction to Relational Databases (RDBMS)',
+    issuer: 'Coursera',
+    date: 'Septiembre 2026',
+    image: rdbmsImg,
+    links: [
+      { label: 'Coursera', icon: 'simple-icons:coursera', url: 'https://coursera.org/share/4a6b1438b139edbfb3901335c50a5816' },
+      { label: 'Credly', icon: 'simple-icons:credly', url: 'https://www.credly.com/badges/b9afaee6-3ac3-4d4f-9701-43d745c04a88/public_url' },
     ],
   },
   {
